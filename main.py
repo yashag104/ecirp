41c7cca3ef6ba6aa621eab16f630e9e8b917066f
yashag104 <161073844+yashag104@users.noreply.github.com>
2024-12-24 13:03:05 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,72 +1,52 @@
 import requests
-from datetime import datetime, timedelta
 from twilio.rest import Client
+import random
+import os
 
-STOCK = "TSLA"
-COMPANY_NAME = "Tesla Inc"
-STOCK_API_KEY = "FYRCRQHV42TOTGLA"
-STOCK_URL = f"https://www.alphavantage.co/query"
-
-ACCOUNT_SID = "ACb74f1548e9b8f0cf752277336ddb9857"
-PHONE = "+15855952560"
-AUTH_TOKEN = "75eebcfa9f2677bfc25ba6d8c1dce877"
-
-NEWS_API_KEY = "6e7bc025ee284856af75ce00a53eef6c"
-NEWS_URL = "https://newsapi.org/v2/everything"
-
-today = datetime.now().date()
-today.strftime("%Y-%m-%d")
-yesterday = today
-day_before_yesterday = today
-if datetime.now().weekday() == 6:
-    yesterday = today - timedelta(days=2)
-    day_before_yesterday = yesterday - timedelta(days=1)
-elif datetime.now().weekday() == 0:
-    yesterday = today - timedelta(days=3)
-    day_before_yesterday = yesterday - timedelta(days=1)
-elif datetime.now().weekday() == 1:
-    yesterday = today - timedelta(days=1)
-    day_before_yesterday = yesterday - timedelta(days=4)
-day_before_yesterday = str(day_before_yesterday.strftime("%Y-%m-%d"))
-yesterday = str(yesterday.strftime("%Y-%m-%d"))
-today = str(today.strftime("%Y-%m-%d"))
-
-
-parameter_news = {
-    "apiKey": NEWS_API_KEY,
-    "qInTitle": COMPANY_NAME,
-    "language": 'en'
-}
-
-parameters_stock = {
-    "function": "TIME_SERIES_DAILY",
-    "symbol": STOCK,
-    "apikey": STOCK_API_KEY,
-}
-
-news_response = requests.get(url=NEWS_URL, params=parameter_news)
-stock_response = requests.get(url=STOCK_URL, params=parameters_stock)
-stock_data = stock_response.json()
-news_data = news_response.json()["articles"]
-
-
-def is_change():
-    percentage_change = ((float(stock_data["Time Series (Daily)"][yesterday]["1. open"]) - float(
-        stock_data["Time Series (Daily)"][day_before_yesterday][
-            "4. close"])) / (float(stock_data["Time Series (Daily)"][yesterday]["1. open"]))) * 100
-    return abs(percentage_change)
-
-
-arrow = None
-if is_change() > 0:
-    up_down = "🔺"
-else:
-    up_down = "🔻"
-
-formatted_articles = [f"{STOCK}: {up_down}{is_change()}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in news_data]
+API_KEY = "b01bbcd6885a746b5ddfdbabfa2f864b"
+ACCOUNT_SID = "AC339409e944224d1ef4b17cb2e6315672"
+PHONE = "+17194276458"
+WHATSAPP = "+14155238886"
+AUTH_TOKEN = "5012b820c4061aab8e733f07ba60933d"
+# LANGUAGE = "en"
+# Locations = {
+#     "damoh": {
+#         "lat": 23.838099,
+#         "lng": 79.442169
+#     },
+#     "pilani": {
+#         "lat": 28.363800,
+#         "lng": 75.600998
+#     },
+#     "indore": {
+#         "lat": 22.719568,
+#         "lng": 75.857727
+#     }
+#
+# }
+#
+# parameters = {
+#     "appid": API_KEY,
+#     "units": "metric",
+#     "lon": Locations["damoh"]["lng"],
+#     "lat": Locations["damoh"]["lat"],
+#     "cnt": 4,
+#     "lang": LANGUAGE
+# }
+#
+# response = requests.get(url=f"https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+# weather = response.json()
+# for entry in weather['list']:
+#     if entry["weather"][0]["id"] < 700:
+#         print("bring an umbrella")
+#sid = os.environ["TWILIO_ACCOUNT_SID"]
+#auth = os.environ["TWILIO_AUTH_TOKEN"]
+with open("quotes.txt") as quote_file:
+    all_quotes = quote_file.readlines()
+    quote = random.choice(all_quotes)
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 message = client.messages.create(
-   body=formatted_articles,
-   from_=PHONE,
-   to="+919479702980"
-   )
+    body="Hi, I am learning python.",
+    from_=PHONE,
+    to="+919479702980"
+)
